70ec956b687dd5c48d4137b47154a4e260242d6b
fady <fadymahros373@gmail.com>
2025-07-17 15:08:48 +0300

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        "./index.html",
+    ],
+    theme: {
+        extend: {
+            colors: {
+
+                Red: 'hsl(0, 78%, 62%)',
+                Cyan: 'hsl(180, 62%, 55%)',
+                Orange: 'hsl(34, 97%, 64%)',
+                Blue: 'hsl(212, 86%, 64%)',
+                Grey500: 'hsl(234, 12%, 34%)',
+                Grey400: 'hsl(212, 6%, 44%)',
+
+            }
+        },
+    },
+    plugins: [],
+};
